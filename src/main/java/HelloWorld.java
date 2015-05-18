b4d7de63708c113e34b581e85bd50e6c9b795d72
dmswlthgus <goddmswl3@naver.com>
2015-05-18 14:13:16 +0900

    조원이름추가

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 
     public static  void  main (String[] args){
 
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello ±è¿¹Áö");
     }
 }
